98ca4027f80d328e2841b4af1694f0d0496ad8af
Shubhankar sinha <141034953+Shubhankarkmr@users.noreply.github.com>
2025-08-28 01:45:28 +0530

    Update index.js

--- server/dbConfig/index.js
+++ server/dbConfig/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const dbConnection = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGODB_URL, {
+    const connection = await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
